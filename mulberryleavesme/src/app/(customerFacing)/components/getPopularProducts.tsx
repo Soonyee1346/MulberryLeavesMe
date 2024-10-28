26ec6116198d5c4e9901e126bc315ced4f871c15
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-10-28 18:50:35 +0900

    Revert "Fixing schema call"
    This reverts commit 3cfb7cd91c30541183dc32ca07c5bf3b2abac223.

--- mulberryleavesme/src/app/(customerFacing)/components/getPopularProducts.tsx
+++ mulberryleavesme/src/app/(customerFacing)/components/getPopularProducts.tsx
@@ -3,43 +3,18 @@
 import db from "@/db/db"
 
 export async function getPopularProducts() {
-    const popularProducts = await db.orderProduct.groupBy({
-        by: ['productId'],
-        _count: {
-            productId: true,
-        },
-        where: {
-            product: {
-                isAvailableForPurchase: true,
-            },
-        },
-        orderBy: {
-            _count: {
-                productId: 'desc',
-            },
-        },
-        take: 4,
-    });
-    
-    // Fetch product details for the top popular products
-    const productIds = popularProducts.map((item) => item.productId);
-    const products = await db.product.findMany({
-        where: {
-            id: { in: productIds },
-        },
-    });
 
-    /*const products = await db.product.findMany({ 
+    const products = await db.product.findMany({ 
         where: { 
             isAvailableForPurchase: true
         }, 
         orderBy: { 
-            order: { 
+            orders: { 
                 _count: "desc"
             }
         },
         take: 4
     })
 
-    return products*/
+    return products
 }
