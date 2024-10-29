a2634cb8d667c181be147e7ceafbeff272fe9855
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-10-29 18:23:21 +0900

    Update page.tsx

--- mulberryleavesme/src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ mulberryleavesme/src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -7,6 +7,9 @@ export default function SuccessPage() {
             <h1 className="mt-6 text-4xl font-bold">Thank You! Your order has been placed!</h1>
             <p className="mt-6 text-lg">The order confirmation email has been sent to your email inbox</p>
             <Badge className="mt-6 text-lg">Order ID: #12345</Badge>
+            <p><b>*** YOUR ORDER HAS NOT BEEN PLACED</b></p>
+            <p><b>*** AN EMAIL HAS NOT BEEN SENT TO YOUR EMAIL INBOX.</b></p>
+            <p><b>*** YOU HAVE NOT BEEN CHARGED</b></p>
         </div>
     )
 }
