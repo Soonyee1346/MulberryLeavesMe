41af276dc5cd8e8d4fc4a4be487a801da4e01820
Soonyee1346 <92239495+Soonyee1346@users.noreply.github.com>
2024-10-29 17:59:54 +0900

    Updating unfinished pages

---
+++ mulberryleavesme/src/app/admin/orders/page.tsx
@@ -0,0 +1,6 @@
+export default function Orders() {
+
+    return <main className="space-y-12">
+        <h1>Page not finished!</h1>
+    </main>
+}
---
+++ mulberryleavesme/src/app/admin/users/page.tsx
@@ -0,0 +1,6 @@
+export default function Sales() {
+
+    return <main className="space-y-12">
+        <h1>Page not finished!</h1>
+    </main>
+}
